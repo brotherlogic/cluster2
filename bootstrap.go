717656084d2241369174ebd8ad67ac20579847bb
Simon <brotherlogic@gmail.com>
2024-11-08 15:32:28 -0800

    Adds basic bootstrap program

---
+++ bootstrap.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+)
+
+func main() {
+	/*
+	** Bootstraps the cluster **
+	 */
+
+	// Can we reach the cluster
+	res, err := exec.Command("kubectl", "get", "nodes").CombinedOutput()
+	if err == nil {
+		count := 0
+		for _, line := range strings.Split(string(res), "\n")[1:] {
+			elems := strings.Fields(line)
+			if elems[0] == "klust1" || elems[0] == "klust2" || elems[0] == "klust3" {
+				count++
+			}
+		}
+
+		// We've seen the three principal nodes
+		if count == 3 {
+			return
+		}
+	}
+
+	fmt.Printf("Cluster setup error\n")
+}
