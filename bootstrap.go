9440f7a2846e070f7c76c6698e31e17192c5b98e
Simon Tucker <brotherlogic@gmail.com>
2024-11-16 08:50:55 -0800

    First round of bootstrap

--- bootstrap.go
+++ bootstrap.go
@@ -1,10 +1,51 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"os"
 	"os/exec"
 	"strings"
+
+	"github.com/google/go-github/v50/github"
+	"golang.org/x/oauth2"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/proto"
 )
+
+const (
+	repo         = "cluster2"
+	user         = "brotherlogic"
+	rebuildTitle = "Request Cluster Rebuild"
+)
+
+func getIssue(ctx context.Context, client *github.Client) (int, error) {
+	issues, _, err := client.Issues.ListByRepo(ctx, user, repo, &github.IssueListByRepoOptions{
+		ListOptions: github.ListOptions{Page: 1},
+	})
+	if err != nil {
+		return -1, err
+	}
+
+	for _, issue := range issues {
+		if issue.GetTitle() == rebuildTitle {
+			return issue.GetNumber(), nil
+		}
+	}
+
+	return -1, status.Error(codes.NotFound, "Rebuild issue not found!")
+}
+
+func createRebuildIssue(ctx context.Context, client *github.Client) {
+	_, _, err := client.Issues.Create(ctx, user, repo, &github.IssueRequest{
+		Title: proto.String(rebuildTitle),
+	})
+	if err != nil {
+		log.Fatalf("Unable to create issue: %v", err)
+	}
+}
 
 func main() {
 	/*
@@ -28,5 +69,28 @@ func main() {
 		}
 	}
 
-	fmt.Printf("Cluster setup error\n")
+	// If we can't reach the cluster, start the bootstrap process
+	//
+	// 1. Load the github client
+	content, err := os.ReadFile("/home/simon/.pat")
+	if err != nil {
+		log.Fatalf("Unable to read pat file: %v", err)
+	}
+
+	ctx := context.Background()
+
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: string(content)},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
+
+	// Check for the issue
+	issue, err := getIssue(ctx, client)
+	if err != nil {
+		createRebuildIssue(ctx, client)
+		return
+	}
+
+	fmt.Printf("Cluster setup error; issue: %v\n", issue)
 }
---
+++ go.mod
@@ -0,0 +1,19 @@
+module github.com/brotherlogic/cluster2
+
+go 1.23.3
+
+require (
+	github.com/google/go-github/v50 v50.2.0
+	golang.org/x/oauth2 v0.24.0
+	google.golang.org/grpc v1.68.0
+	google.golang.org/protobuf v1.35.2
+)
+
+require (
+	github.com/ProtonMail/go-crypto v1.1.2 // indirect
+	github.com/cloudflare/circl v1.5.0 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	golang.org/x/crypto v0.29.0 // indirect
+	golang.org/x/sys v0.27.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20241113202542-65e8d215514f // indirect
+)
